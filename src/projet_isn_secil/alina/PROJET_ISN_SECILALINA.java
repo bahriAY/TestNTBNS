24a94bd3f697a080c927a36540857f69537cdcc4
bahriay <bahriay@192.168.1.33>
2018-05-02 14:50:36 +0200

    test

---
+++ src/projet_isn_secil/alina/PROJET_ISN_SECILALINA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_isn_secil.alina;
+
+/**
+ *
+ * @author bahriay
+ */
+public class PROJET_ISN_SECILALINA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }    
+}
